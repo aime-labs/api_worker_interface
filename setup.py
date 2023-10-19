5622d8c4a9d480709dd6f6ed65adad2ab005cefe
carlo <carlo@aime.info>
2023-10-19 18:57:52 +0200

    Bugfix version no.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='api worker interface',
-      version='0.1.4',
+      version='0.2.0',
       author='Carlo',
       author_email='carlo@aime.info',
       packages=['api_worker_interface'],
