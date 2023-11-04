b0e7c6b55492b47933820f6d78434ad8e33f89fc
carlo <carlo@aime.info>
2023-11-04 17:59:42 +0100

    Updated docstrings

--- api_worker_interface/api_worker_interface.py
+++ api_worker_interface/api_worker_interface.py
@@ -24,8 +24,58 @@ class MyManager(SyncManager):
 
 
 class APIWorkerInterface():
-    """Interface for deep learning models to communicate with AIME-ML-API
+    f"""Interface for deep learning models to communicate with AIME-ML-API
+
+    Args:
+        api_server (str): Address of API Server. Example: 'http://api.aime.team'.
+        job_type (str): Type of job . Example: "stable_diffusion_xl_txt2img".
+        auth_key (str): key to authorize worker to connect with API Server.
+        gpu_id (int, optional): ID of GPU the worker runs on. Defaults to 0.
+        world_size (int, optional): Number of used GPUs the worker runs on. Defaults to 1.
+        rank (int, optional): ID of current GPU if world_size > 1. Defaults to 0.
+        gpu_name (str, optional): Name of GPU the worker runs on. Defaults to None.
+        progress_received_callback (function, optional): Callback function with no arguments, called when API server received all data from send_progress(..). Defaults to None.
+        image_metadata_params (list, optional): Parameters to add as metadata to images (Currently only 'PNG'). Defaults to {DEFAULT_IMAGE_METADATA}.
+
+    Example usage:
+        
+        api_worker = APIWorkerInterface()
+        while True:
+            job_data = api_worker.job_request('http://api.aime.team', 'llama2_chat', )
+            output = do_deep_learning_worker_calculations()
+            api_worker.send_job_results(job_data, output)
+
+    Example usage with progress:
+        api_worker = APIWorkerInterface()
+        while True:
+            job_data = api_worker.job_request()
+            
+            for step in deep_learning_worker_calculation:
+                progress_in_percent = round(step*100/len(deep_learning_worker_calculation))
+                progress_data = do_deep_learning_worker_calculation_step()
+                if api_worker.progress_data_received:
+                    api_worker.send_progress(job_data, progress_in_percent, progress_data)
+            output = get_result()
+            api_worker.send_job_results(job_data, output)
+    Example usage with progress callbacks:
+        def progress_callback(progress, progress_data):
+            api_worker.send_progress(job_data, progress_in_percent, progress_data)
+
+
+
+
+        api_worker = APIWorkerInterface()
+        while True:
+            job_data = api_worker.job_request()
+            
+
+
+
+
+
+
     """    
+
 
     manager = None
     barrier = None
@@ -35,16 +85,18 @@ class APIWorkerInterface():
         return APIWorkerInterface.barrier
 
     def __init__(self, api_server, job_type, auth_key, gpu_id=0, world_size=1, rank=0, gpu_name=None, progress_received_callback=None, image_metadata_params=DEFAULT_IMAGE_METADATA):
-        """Constructor
-
+        f"""Constructor
         Args:
-            api_server (str): Address of API Server. Example: 'http://api.aime.team'
-            job_type (str): Type of job . Example: "stable_diffusion_xl_txt2img"
-            auth_key (str): key to authorize worker to connect with API Server
+            api_server (str): Address of API Server. Example: 'http://api.aime.team'.
+            job_type (str): Type of job . Example: "stable_diffusion_xl_txt2img".
+            auth_key (str): key to authorize worker to connect with API Server.
             gpu_id (int, optional): ID of GPU the worker runs on. Defaults to 0.
             world_size (int, optional): Number of used GPUs the worker runs on. Defaults to 1.
             rank (int, optional): ID of current GPU if world_size > 1. Defaults to 0.
             gpu_name (str, optional): Name of GPU the worker runs on. Defaults to None.
+            progress_received_callback (function, optional): Callback function with no arguments, called when API server received all data from send_progress(..). Defaults to None.
+            image_metadata_params (list, optional): Parameters to add as metadata to images (Currently only 'PNG'). Defaults to {DEFAULT_IMAGE_METADATA}
+
         """        
         self.api_server = api_server
         self.job_type = job_type
@@ -78,10 +130,10 @@ class APIWorkerInterface():
 
 
     def __make_worker_name(self):
-        """Make a name for the worker based on gpu_id and gpu_name
+        """Make a name for the worker: 
 
         Returns:
-            str: created name of worker
+            str: name of the worker like <hostname>_<gpu_name>_<gpu_id> if gpu_name is given, , else <hostname>_GPU_<gpu_id>
         """        
         worker_name = socket.gethostname()
         for id in range(self.world_size):
@@ -97,7 +149,7 @@ class APIWorkerInterface():
         """Worker requests a job from API Server on route /worker_job_request
 
         Returns:
-            dict: job_data with worker [INPUT] parameters
+            dict: job_data with worker [INPUT] parameters received from API server
         """        
         job_data = None
         if self.rank == 0:
@@ -138,18 +190,22 @@ class APIWorkerInterface():
         """Process/convert job results and send it to API Server on route /worker_job_result
 
         Args:
-            job_data (dict): worker [INPUT] parameters 
-            results (dict): worker result parameters ('image', 'images' or 'text')
+            job_data (dict): worker [INPUT] parameters, returned from job_request()
+            results (dict): worker [OUTPUT] result parameters (f.i. 'image', 'images' or 'text')
 
         Returns:
-            Response: Response to the worker 
+            requests.models.Response: Response from API server to the worker, 
+            Example_response.json() : 
+                Server received data without problems:          {'cmd': 'ok'} 
+                An error occured in API server:                 {'cmd': 'error', 'msg': <error message>} 
+                Server received data received with a warning:   {'cmd': 'warning', 'msg': <warning message>}
         """
         response = None
         if self.rank == 0:        
             for parameter in ['job_id', 'start_time', 'start_time_compute', 'auth']:
                 results[parameter] = job_data.get(parameter)
 
-            results = self.__convert_output_types_to_string_representation(job_data, results, True)
+            results = self.__convert_output_types_to_string_representation(results, job_data, True)
             try:
                 response = self.__fetch('/worker_job_result', results)
             except requests.exceptions.ConnectionError:
@@ -159,61 +215,60 @@ class APIWorkerInterface():
 
 
     def send_progress(self, job_data, progress, progress_data=None):
-        """Process/convert job progress information and data while worker is computing and send it to API Server on route /worker_job_progress
+        """Process/convert job progress information and data and send it to API Server on route /worker_job_progress asynchronously
 
         Args:
             job_data (dict): worker [INPUT] parameters 
             progress (int): current progress (f.i. percent or number of generated tokens)
             progress_data (dict, optional): dictionary with progress_images or text while worker is computing. Defaults to None.
-
-        Returns:
-            Response: Response to the worker
+            Example progress data:
+                {'image': PIL.PngImagePlugin.PngImageFile }
         """
+        print('#############', progress_data)
         response = None
         if self.rank == 0:        
             payload = {'progress': progress, 'job_id': job_data['job_id']}
-            payload['progress_data'] = self.__convert_output_types_to_string_representation(job_data, progress_data, False)
+            payload['progress_data'] = self.__convert_output_types_to_string_representation(progress_data, job_data, False)
             self.progress_data_received = False
-            response = self.__fetch_async('/worker_job_progress', payload)
-        return response
+            _ = self.__fetch_async('/worker_job_progress', payload)
 
 
-    def __convert_output_types_to_string_representation(self, job_data, output, finished):
-        """Process/convert job progress information and data while worker is computing and send it to API Server on route /worker_job_progress
+    def __convert_output_types_to_string_representation(self, output_data, job_data, finished):
+        """Converts parameters output data from type 'image' and 'image_list' to base64 strings. 
 
         Args:
+            output_data (dict): worker [OUTPUT] parameter dictionary
             job_data (dict): worker [INPUT] parameters 
-            output (dict): current progress (f.i. percent or number of generated tokens)
-            finished (bool): Set True if end result, False if progress_data
+            finished (bool): Set True fro sending end result, False for progress data
 
         Returns:
-            dict: Response to the worker
+            dict: worker [OUTPUT] parameter dictionary with data converted to base64 string
         """
 
         
-        if output:
+        if output_data:
             mode = 'output' if finished else 'progress'
             descriptions = job_data.get(f'{mode}_descriptions')
             for output_name, output_description in descriptions.items():
-                if output_name in output:
+                if output_name in output_data:
                     output_type = output_description.get('type') 
                     if output_type == 'image':
-                        output[output_name] = self.__convert_image_to_base64_string(
-                            output[output_name], output_description.get('image_format', 'PNG'), job_data)
+                        output_data[output_name] = self.__convert_image_to_base64_string(
+                            output_data[output_name], output_description.get('image_format', 'PNG'), job_data)
                     elif output_type == 'image_list':
-                        output[output_name] = self.__convert_image_list_to_base64_string(
-                            output[output_name], output_description.get('image_format', 'PNG'), job_data)
+                        output_data[output_name] = self.__convert_image_list_to_base64_string(
+                            output_data[output_name], output_description.get('image_format', 'PNG'), job_data)
                 if finished:
-                    if output_name in job_data and output_name not in output:
-                        output[output_name] = job_data[output_name]
-        return output
+                    if output_name in job_data and output_name not in output_data:
+                        output_data[output_name] = job_data[output_name]
+        return output_data
 
 
     def __convert_image_to_base64_string(self, image, image_format, job_data):
         """Converts given PIL image to base64 string with given image_format and image metadata parsed from given job_data.
 
         Args:
-            image (PIL): Python pillow image to be converted
+            image (PIL.PngImagePlugin.PngImageFile): Python pillow image to be converted
             image_format (str): Image format. f.i. 'PNG', 'JPG'
             job_data (dict): Dictionary containing image metadata
 
@@ -235,7 +290,7 @@ class APIWorkerInterface():
         """Converts given list of PIL images to base64 string with given image_format and image metadata parsed from given job_data.
 
         Args:
-            list_images (list): List of python pillow images to be converted
+            list_images (list [PIL.PngImagePlugin.PngImageFile, ..]): List of python pillow images to be converted
             image_format (str): Image format. f.i. 'PNG', 'JPG'
             job_data (dict): Dictionary containing image metadata
 
